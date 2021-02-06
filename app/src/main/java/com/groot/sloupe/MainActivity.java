246b079f99bbdbf54e1970abcd6310e7f5942faf
ihormaniuk <ihor.maniuk@gmail.com>
2021-02-06 10:55:46 +0200

    Initial commit

---
+++ app/src/main/java/com/groot/sloupe/MainActivity.java
@@ -0,0 +1,23 @@
+package com.groot.sloupe;
+
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        recyclerView.findViewById(R.id.recyclerview);
+//        textView.findViewById(R.id.textview1);
+//        LinearLayoutManager linearSmoothScroller = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(linearSmoothScroller);
+    }
+}
