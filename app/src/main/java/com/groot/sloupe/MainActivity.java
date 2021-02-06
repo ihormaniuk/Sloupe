c41ff1141316994c7c71e863c9fb1f95c89cede5
ihor.maniuk <ihor.maniuk@gmail.com>
2021-02-06 12:32:33 +0200

    master

--- app/src/main/java/com/groot/sloupe/MainActivity.java
+++ app/src/main/java/com/groot/sloupe/MainActivity.java
@@ -15,9 +15,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        recyclerView.findViewById(R.id.recyclerview);
-//        textView.findViewById(R.id.textview1);
-//        LinearLayoutManager linearSmoothScroller = new LinearLayoutManager(this);
-//        recyclerView.setLayoutManager(linearSmoothScroller);
     }
 }
